ce4c08ce5eed6da5414792b9588bbc8da2184928
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2022-11-19 15:06:44 +0530

    Using useStates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React , {useState} from 'react';
 
 function App() {
+  const [count , setCount] = useState(5);
+
+  function incrementCount() {
+    setCount(previousCount => previousCount + 1);
+  }
+  
+  function decrementCount() {
+    setCount(previousCount => previousCount - 1);
+  }
+
   return (
     <>
-      <button>+</button>
-      <span>0</span>
-      <button>-</button>
+      <button onClick={incrementCount}>+</button>
+      <span>{count}</span>
+      <button onClick={decrementCount}>-</button>
     </>
   )
 }
